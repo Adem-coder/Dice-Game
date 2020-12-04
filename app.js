6a74602554293f7044ed7ca17e2a44d34dea62ac
Ashu <ashutolosa3@gmail.com>
2020-12-05 01:15:13 +0300

    Finishing Touches State Variables

--- app.js
+++ app.js
@@ -9,28 +9,34 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
-    var dice = Math.floor(Math.random() * 6) + 1;
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
-    if (dice !== 1) {
-    
-        roundScore += dice;
-   
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-       nextPlayer();
+    if (gamePlaying) {
+
+        var dice = Math.floor(Math.random() * 6) + 1;
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        if (dice !== 1) {
+        
+            roundScore += dice;
+       
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+           nextPlayer();
+        }
     }
+
+   
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-scores[activePlayer] += roundScore;
+if (gamePlaying) {
+    scores[activePlayer] += roundScore;
 
 document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
@@ -39,11 +45,12 @@ if(scores[activePlayer] >= 20) {
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-'+ activePlayer+ '-panel').classList.add('winner');
     document.querySelector('.player-'+ activePlayer+ '-panel').classList.remove('active');
- 
+ gamePlaying = false;
 
 } else{
     nextPlayer();
     
+}
 }
 
 
@@ -68,6 +75,8 @@ function init() {
     scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
+
+gamePlaying = true;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
